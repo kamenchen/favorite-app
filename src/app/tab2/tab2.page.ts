a23f1bc73340e4f39226a4e7a77fcb13c8ef9a56
Kamen Chen Yu <Kamen.ChenYu01@student.csulb.edu>
2025-09-16 14:59:23 -0700

    change 2

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import dishes from 'src/assets/data/dishes';
+import { Dishes } from 'src/assets/data/dishes.interface';
 
 @Component({
   selector: 'app-tab2',
@@ -6,8 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss'],
   standalone: false,
 })
-export class Tab2Page {
-
+export class Tab2Page implements OnInit {
+  dishCollection: {category: string, dishes: Dishes[]}[];
   constructor() {}
+
+  ngOnInit() {
+    this.dishCollection = dishes;
+    console.log(this.dishCollection);
+  }
 
 }
